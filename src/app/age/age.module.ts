4245b94f4c03e02238918a230f3d9f5a0d5be141
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-23 12:05:23 +0530

    feat(core): create age module,age component and imported in app module

---
+++ src/app/age/age.module.ts
@@ -0,0 +1,17 @@
+//Angular Imports
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+//Components
+import { AgeComponent } from './age.component';
+
+
+
+@NgModule({
+  declarations: [
+    AgeComponent
+  ],
+  imports: [
+    CommonModule,
+  ]
+})
+export class AgeModule { }
