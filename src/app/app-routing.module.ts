daf70e8c654f12c5d62574121e9cd66d4d86d662
Aasthasree <sreelakshmi7032@gmail.com>
2024-02-23 16:17:27 +0530

    feat(app-routing): add route for age component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
+//Angular Imports
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+//Components
+import { AgeComponent } from './age/age.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: AgeComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
